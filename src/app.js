91276a5111975cacf625c661babedd4337151efe
bhargavActyv <bhargav.katira@actyv.com>
2024-07-29 13:43:06 +0530

    task-manageent-api

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoutes = require('./routes/user');
+const taskRoutes = require('./routes/task');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, { });
+
+
+app.use('/api/users', userRoutes);
+app.use('/api/tasks', taskRoutes);
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({ message: err.message });
+});
+
+module.exports = app;
